48a2ed720124a5f1ace999087fa954ff308a75a4
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2022-08-05 09:37:16 -0300

    calculation and general modifications

--- libs/frames/main_frame.py
+++ libs/frames/main_frame.py
@@ -1,9 +1,6 @@
 
-import pydicom
 import os
 import numpy as np
-import datetime as dt
-import cv2 
 import random
 
 from tkinter import Frame, Label, Button, Entry, filedialog, END, Toplevel, \
@@ -20,26 +17,6 @@ class MainFrame(Frame):
 
         self.root = root
 
-        root_init_2afc = Toplevel(self.root)
-
-        root_init_2afc.title("Info")
-        root_init_2afc.geometry("305x452")
-        root_init_2afc.minsize(width=305, height=452)
-        root_init_2afc.maxsize(width=305, height=452)
-
-        infoFrame = info_frame(root_init_2afc, self)
-
-        root_init_2afc.protocol("WM_DELETE_WINDOW", infoFrame.on_closing)
-        
-        ##########################################
-        # Frames
-        ##########################################
-
-        self.build_frames()
-
-        self.build_top_frame()
-        self.build_btm_frame()
-
         ##########################################
         # Variables
         ##########################################
@@ -54,19 +31,44 @@ class MainFrame(Frame):
         
         self.real_imgs_paths = self.real_imgs_paths[:100]
         self.fake_imgs_paths = self.fake_imgs_paths[:100]
-        # print(len(self.real_imgs_paths),len(self.fake_imgs_paths))
+        #print(len(self.real_imgs_paths),len(self.fake_imgs_paths))
+
         self.where_is_fake = []
         self.where_user_clicked = []
         
         self.img_on_left = None
         self.img_on_right = None
 
+        self.flag_info_ok = 0
+        self.user_folder = None
+        
+        ##########################################
+        # Frames
+        ##########################################
+
+        self.build_frames()
+
         ##########################################
         # Startup functions
         ##########################################
-        
-        self.get_next_images()
 
+        ##########################################
+        # Info FRAME
+        ##########################################
+
+        root_init_2afc = Toplevel(self.root)
+
+        root_init_2afc.title("Info")
+        # root_init_2afc.geometry("305x452")
+        # root_init_2afc.minsize(width=260, height=150)
+        # root_init_2afc.maxsize(width=260, height=150)
+
+        infoFrame = info_frame(root_init_2afc, self)
+
+        root_init_2afc.protocol("WM_DELETE_WINDOW", infoFrame.on_closing)
+
+        return
+        
 
     def build_frames(self):
 
@@ -76,6 +78,9 @@ class MainFrame(Frame):
         # Palce it on a grid
         self.top_frame.grid(row=0)
         self.btm_frame.grid(row=1)
+
+        self.build_top_frame()
+        self.build_btm_frame()
         
 
     def build_top_frame(self):
@@ -117,26 +122,30 @@ class MainFrame(Frame):
 
 
     def func_button_left(self):
+
+        if self.flag_info_ok:
         
-        self.where_user_clicked.append(1)
-        # print(self.where_user_clicked, self.where_is_fake)
-        # if self.where_user_clicked[-1] == self.where_is_fake[-1]:
-        #     print("Wrong")
-        # else:
-        #     print("Correct")
-        self.get_next_images()
+            self.where_user_clicked.append(1)
+            # print(self.where_user_clicked, self.where_is_fake)
+            # if self.where_user_clicked[-1] == self.where_is_fake[-1]:
+            #     print("Wrong")
+            # else:
+            #     print("Correct")
+            self.get_next_images()
 
         return
 
     def func_button_right(self):
+
+        if self.flag_info_ok:
     
-        self.where_user_clicked.append(0)
-        # print(self.where_user_clicked, self.where_is_fake)
-        # if self.where_user_clicked[-1] == self.where_is_fake[-1]:
-        #     print("Wrong")
-        # else:
-        #     print("Correct")
-        self.get_next_images()
+            self.where_user_clicked.append(0)
+            # print(self.where_user_clicked, self.where_is_fake)
+            # if self.where_user_clicked[-1] == self.where_is_fake[-1]:
+            #     print("Wrong")
+            # else:
+            #     print("Correct")
+            self.get_next_images()
 
         return
     
@@ -167,42 +176,77 @@ class MainFrame(Frame):
     def get_next_images(self):
         
         # print(self.real_imgs_paths[self.img_ind], self.fake_imgs_paths[self.img_ind])
-        
-        img_real = readDicom(self.real_imgs_paths[self.img_ind])
-        img_fake = readDicom(self.fake_imgs_paths[self.img_ind])
-        
-        img_real = self.pre_process(img_real)
-        img_fake = self.pre_process(img_fake)
-        
-        if np.random.uniform() > 0.5:
-            # Show fake on left
-            self.where_is_fake.append(1)
-            self.img_on_left = img_fake
-            self.img_on_right = img_real
-            self.show_img(self.img_on_left[..., 7], self.img_panel_left)
-            self.show_img(self.img_on_right[..., 7], self.img_panel_right)
+
+        if self.img_ind == 100:
+            self.calc_statistcs()
+            self.on_quit()
         else:
-            # Show fake on right
-            self.where_is_fake.append(0)
-            self.img_on_left = img_real
-            self.img_on_right = img_fake
-            self.show_img(self.img_on_left[..., 7], self.img_panel_left)
-            self.show_img(self.img_on_right[..., 7], self.img_panel_right)
+            img_real = readDicom(self.real_imgs_paths[self.img_ind])
+            img_fake = readDicom(self.fake_imgs_paths[self.img_ind])
             
-        self.left_scrollbar.set(7)
-        self.right_scrollbar.set(7)
+            img_real = self.pre_process(img_real)
+            img_fake = self.pre_process(img_fake)
             
-        self.img_ind += 1
+            if np.random.uniform() > 0.5:
+                # Show fake on left
+                self.where_is_fake.append(1)
+                self.img_on_left = img_fake
+                self.img_on_right = img_real
+                self.show_img(self.img_on_left[..., 7], self.img_panel_left)
+                self.show_img(self.img_on_right[..., 7], self.img_panel_right)
+            else:
+                # Show fake on right
+                self.where_is_fake.append(0)
+                self.img_on_left = img_real
+                self.img_on_right = img_fake
+                self.show_img(self.img_on_left[..., 7], self.img_panel_left)
+                self.show_img(self.img_on_right[..., 7], self.img_panel_right)
+                
+            self.left_scrollbar.set(7)
+            self.right_scrollbar.set(7)
+                
+            self.img_ind += 1
    
         return
     
     def get_next_slice(self, pos, name):
+
+        if self.flag_info_ok:
         
-        if name == 'left':
-            self.show_img(self.img_on_left[..., int(pos)], self.img_panel_left)
-        else:
-            self.show_img(self.img_on_right[..., int(pos)], self.img_panel_right)
+            if name == 'left':
+                self.show_img(self.img_on_left[..., int(pos)], self.img_panel_left)
+            else:
+                self.show_img(self.img_on_right[..., int(pos)], self.img_panel_right)
             
+        return
+
+    def calc_statistcs(self):
+
+        # Question: Where is the Real?
+        #  --- where_user_clicked ---
+        # Button Left = 1
+        # Button Right = 0
+        #  --- where_is_fake ---
+        # Fake Left = 1
+        # Fake Right = 0
+        # ---------
+        # Example:
+        #
+        # where_user_clicked = [0, 1, 0]
+        # where_is_fake      = [1, 1, 0]
+        # Result             = [False, True, True]
+
+        numer_of_correct = np.size(np.where(np.array(self.where_user_clicked) ^ np.array(self.where_is_fake) == 1)[0])
+
+        with open(os.path.join(self.user_folder,'results.txt'), 'a') as f:
+                f.write("Percentage of correct choices: {}".format(numer_of_correct / 100))
+
+        return
+
+    def on_quit(self):
+
+        self.root.quit()
+
         return
 
 
