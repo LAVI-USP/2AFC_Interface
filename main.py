d1d11e4ab0b931d16ece45547675e95731b14db5
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2022-08-05 13:01:04 -0300

    change work dir

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+
 from tkinter import Menu, Tk
 
 from libs.frames.main_frame import MainFrame
+
+# Ser working dir to the one of main.py
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 root = Tk()
 
